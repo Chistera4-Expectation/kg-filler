0c12ffb56d3deded2f9236cbed2fc520dc94ef7b
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-05-16 19:09:51 +0200

    fix: queries for instances

--- kgfiller/__main__.py
+++ kgfiller/__main__.py
@@ -8,25 +8,27 @@ enable_logging()
 
 
 instance_queries = [
+    f"instances list for class {CLASS_NAME_FANCY}, names only",
     f"instances list for class {CLASS_NAME_FANCY}",
+    f"examples list for {CLASS_NAME_FANCY}, names only",
     f"examples list for {CLASS_NAME_FANCY}",
 ]
 
 
 recipe_queries = [
-    f"ingredient list for {INSTANCE_NAME_FANCY}",
+    f"ingredient list for {INSTANCE_NAME_FANCY}, names only",
 ]
 
 
 with DataRepository() as repo:
     with KnowledgeGraph() as kg:
-        # for cls in kg.visit_classes_depth_first():
-        #     commit = find_instances_for_class(kg, cls, instance_queries)
+        for cls in kg.visit_classes_depth_first():
+            commit = find_instances_for_class(kg, cls, instance_queries)
+            kg.save()
+            repo.maybe_commit(commit)
+        #     # input("Press enter to continue...")
+        # for instance in kg.onto.Recipe.instances():
+        #     commit = find_related_instances(kg, instance, kg.onto.ingredientOf, kg.onto.Edible, recipe_queries)
         #     kg.save()
         #     repo.maybe_commit(commit)
         #     # input("Press enter to continue...")
-        for instance in kg.onto.Recipe.instances():
-            commit = find_related_instances(kg, instance, kg.onto.ingredientOf, kg.onto.Edible, recipe_queries)
-            kg.save()
-            repo.maybe_commit(commit)
-            # input("Press enter to continue...")
