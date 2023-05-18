85c91b6e381a0b5fcdf3067023a5d28d21de87c3
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-05-18 15:25:54 +0200

    chore: factorize code in strategies

--- kgfiller/strategies.py
+++ kgfiller/strategies.py
@@ -1,9 +1,12 @@
-from kgfiller import logger, Commitable, Commit
-from kgfiller.utils import first_or_none
-from kgfiller.kg import KnowledgeGraph, human_name
-from kgfiller.ai import ai_query
-import owlready2 as owlready
 import typing
+
+import owlready2 as owlready
+
+from kgfiller import logger, Commitable, Commit
+from kgfiller.ai import ai_query, AiQuery
+from kgfiller.kg import KnowledgeGraph, human_name
+from kgfiller.text import Item
+from kgfiller.utils import first_or_none
 
 
 CLASS_NAME = "__CLASS_NAME__"
@@ -16,115 +19,213 @@ CLASS_LIST = "__CLASS_LIST__"
 CLASS_LIST_FANCY = "__CLASS_LIST_FANCY_"
 
 
-def _apply_replacements(pattern: str, replacements: typing.Dict[str, str]) -> str:
+DEFAULT_MAX_RETRIES = 2
+
+
+def _apply_replacements(pattern: str, **replacements) -> str:
     for key, value in replacements.items():
         pattern = pattern.replace(key, value)
     return pattern
 
 
-def find_instances_for_class(kg: KnowledgeGraph, cls: owlready.ThingClass, queries: typing.List[str], max_retries: int = 2) -> Commitable:
+class QueryProcessor(Commit):
+    def __init__(self):
+        Commit.__init__(self, "", [], "", True)
+        self._kg = None
+
+    def reset(self, kg: KnowledgeGraph):
+        self._kg = kg
+        self.description = ""
+        self.files = [kg.path]
+        self.message = ""
+        self.should_commit = True
+
+    def final_message(self, kg: KnowledgeGraph, query: AiQuery, *results) -> str:
+        raise NotImplementedError()
+
+    def admissible(self, kg: KnowledgeGraph, query: AiQuery) -> bool:
+        raise NotImplementedError()
+
+    def process(self, kg: KnowledgeGraph, query: AiQuery):
+        raise NotImplementedError()
+
+    def __call__(self, query: AiQuery) -> bool:
+        self.files.append(query.cache_path)
+        if not self.admissible(self._kg, query):
+            return False
+        results = self.process(self._kg, query)
+        self.message = self.final_message(self._kg, query, *results)
+        if not self.message:
+            raise ValueError("No message set for query")
+        if self.description:
+            self.description = self.description.strip()
+        return True
+
+    def describe(self, msg: str, prefix='\n', suffix=''):
+        self.description += prefix + msg + suffix
+
+    def describe_caches(self):
+        self.describe("Query cache in files:")
+        for file in self.files[1:]:
+            self.describe(f"- {str(file)}")
+
+
+class MultipleResultsQueryProcessor(QueryProcessor):
+    def __init__(self):
+        super().__init__()
+        self._results = []
+
+    def admissible(self, kg: KnowledgeGraph, query: AiQuery) -> bool:
+        self._results = query.result_to_list()
+        return len(self._results) > 0
+
+    def process(self, kg: KnowledgeGraph, query: AiQuery):
+        self.describe(f"Query: {query.question}.\nAnswers:")
+        results = []
+        for result in self._results:
+            r = self.process_result(kg, query, result)
+            if isinstance(r, typing.Iterable):
+                results.extend(r)
+            else:
+                results.append(r)
+        return results
+
+    def process_result(self, kg: KnowledgeGraph, query: AiQuery, result: Item) -> typing.Any:
+        raise NotImplementedError()
+
+
+class SingleResultQueryProcessor(QueryProcessor):
+    def __init__(self):
+        super().__init__()
+        self._result = None
+
+    def admissible(self, kg: KnowledgeGraph, query: AiQuery) -> bool:
+        self._result = self.parse_result(kg, query)
+        return self._result is not None
+
+    def parse_result(self, kg: KnowledgeGraph, query: AiQuery) -> typing.Any:
+        raise NotImplementedError()
+
+    def process(self, kg: KnowledgeGraph, query: AiQuery):
+        self.describe(f"Query: {query.question}.\nAnswer:\n\t{query.result_text}")
+        return self.process_result(kg, query, self._result)
+
+    def process_result(self, kg: KnowledgeGraph, query: AiQuery, result: typing.Any) -> typing.Any:
+        raise NotImplementedError()
+
+
+def _make_queries(kg: KnowledgeGraph,
+                  queries: typing.List[str],
+                  query_processor: QueryProcessor,
+                  max_retries: int,
+                  **replacements) -> Commitable:
+    questions = [_apply_replacements(pattern, **replacements) for pattern in queries]
+    query_processor.reset(kg)
+    for question in questions:
+        for attempt in range(0, max_retries):
+            query = ai_query(question=question, attempt=attempt if attempt > 0 else None)
+            if not query_processor(query):
+                logger.warning("No results for query '%s', AI answer: %s", query.question, query.result_text)
+                continue
+            query_processor.describe_caches()
+            return query_processor
+
+
+def find_instances_for_class(kg: KnowledgeGraph,
+                             cls: owlready.ThingClass,
+                             queries: typing.List[str],
+                             max_retries: int = DEFAULT_MAX_RETRIES) -> Commitable:
+    class FindInstancesQueryProcessor(MultipleResultsQueryProcessor):
+        def final_message(self, kg: KnowledgeGraph, query: AiQuery, *results) -> str:
+            return f"add {len(results)} instances to class {cls.name} from AI answer"
+
+        def process_result(self, kg: KnowledgeGraph, query: AiQuery, result: Item):
+            instance = kg.add_instance(cls, result.value)
+            self.describe(f"- {result} => adding instance {instance.name} to class {cls.name}")
+            return instance
+
     replacements = {
         CLASS_NAME: cls.name,
         CLASS_NAME_FANCY: human_name(cls),
     }
-    questions = [_apply_replacements(pattern, replacements) for pattern in queries]
-    for question in questions:
-        files = [kg.path]
-        for attempt in range(0, max_retries):
-            query = ai_query(question=question, attempt=attempt if attempt > 0 else None)
-            results = list(query.result_to_list())
-            if len(results) == 0:
-                logger.warning("No results for query '%s', AI answer: %s", query.question, query.result_text)
-                continue
-            description = f"Query: {query.question}.\nAnswers:"
-            for result in results:
-                instance = kg.add_instance(cls, result.value)
-                description += f"\n- {result} => adding instance {instance.name} to class {cls.name}"
-            files.append(query.cache_path)
-            description += f"\nQuery cache in files:"
-            for file in files[1:]:
-                description += f"\n- {str(file)}"
-            return Commit(
-                message=f"add {len(results)} instances to class {cls.name} from AI answer",
-                description=description,
-                files=files,
-            )
+    return _make_queries(kg, queries, FindInstancesQueryProcessor(), max_retries=max_retries, **replacements)
 
 
-def find_related_instances(kg: KnowledgeGraph, instance: owlready.Thing, relation: owlready.ObjectPropertyClass, default_class: owlready.ThingClass, queries: typing.List[str], instance_as_object: bool = False, max_retries: int = 2) -> Commitable:
+def find_related_instances(kg: KnowledgeGraph,
+                           instance: owlready.Thing,
+                           relation: owlready.ObjectPropertyClass,
+                           default_class: owlready.ThingClass,
+                           queries: typing.List[str],
+                           instance_as_object: bool = False,
+                           max_retries: int = DEFAULT_MAX_RETRIES) -> Commitable:
+    class FindRelatedInstancesQueryProcessor(MultipleResultsQueryProcessor):
+        def final_message(self, kg: KnowledgeGraph, query: AiQuery, *results) -> str:
+            return f"add {len(results)} instances to class {default_class.name}, and as many relations " \
+                   f"to instance {instance.name} from AI answer"
+
+        def process_result(self, kg: KnowledgeGraph, query: AiQuery, result: Item):
+            results = []
+            for word in result.split_by_words():
+                new_instance = kg.add_instance(default_class, word)
+                if instance_as_object:
+                    kg.add_property(new_instance, relation, instance)
+                else:
+                    kg.add_property(instance, relation, new_instance)
+                self.describe(f"- {result} => adding instance {new_instance.name} to class {default_class.name}, and "
+                              f"relate it to {instance.name} as {relation.name}")
+                results.append(new_instance)
+            return results
+
     replacements = {
         INSTANCE_NAME: instance.name,
         INSTANCE_NAME_FANCY: human_name(instance),
         RELATION_NAME: relation.name,
         RELATION_NAME_FANCY: human_name(relation),
     }
-    questions = [_apply_replacements(pattern, replacements) for pattern in queries]
-    for question in questions:
-        files = [kg.path]
-        for attempt in range(0, max_retries):
-            query = ai_query(question=question, attempt=attempt if attempt > 0 else None)
-            results = list(query.result_to_list())
-            if len(results) == 0:
-                logger.warning("No results for query '%s', AI answer: %s", query.question, query.result_text)
-                continue
-            description = f"Query: {query.question}.\nAnswers:"
-            for result in results:
-                for word in result.split_by_words():
-                    new_instance = kg.add_instance(default_class, word)
-                    if instance_as_object:
-                        kg.add_property(new_instance, relation, instance)
-                    else:
-                        kg.add_property(instance, relation, new_instance)
-                    description += f"\n- {word} => adding instance {new_instance.name} to class {default_class.name}, and relate it to {instance.name} as {relation.name}"
-            files.append(query.cache_path)
-            description += f"\nQuery cache in files:"
-            for file in files[1:]:
-                description += f"\n- {str(file)}"
-            return Commit(
-                message=f"add {len(results)} instances to class {default_class.name}, and as many relations to instance {instance.name} from AI answer",
-                description=description,
-                files=files,
-            )
+    return _make_queries(kg, queries, FindRelatedInstancesQueryProcessor(), max_retries=max_retries, **replacements)
 
 
-def move_to_most_adequate_subclass(kg: KnowledgeGraph, instance: owlready.Thing, root_class: owlready.ThingClass, queries: typing.List[str], max_retries: int = 2) -> Commitable:
+def move_to_most_adequate_class(kg: KnowledgeGraph,
+                                instance: owlready.Thing,
+                                classes: typing.Iterable[owlready.ThingClass],
+                                queries: typing.List[str],
+                                max_retries: int = DEFAULT_MAX_RETRIES) -> Commitable:
+    class MoveToMostAdequateClassQueryProcessor(SingleResultQueryProcessor):
+        def process(self, kg: KnowledgeGraph, query: AiQuery):
+            super().process(kg, query)
+            self.message = f"make {instance.name} an instance of classes {[cls.name for cls in instance.is_a]} " \
+                           f"from AI answer",
+
+        def parse_result(self, kg: KnowledgeGraph, query: AiQuery) -> typing.Any:
+            return first_or_none(cls for name, cls in sub_types_by_name.items() if name in query.result_text)
+
+        def process_result(self, kg: KnowledgeGraph, query: AiQuery, result: typing.Any):
+            self.describe(f"meaning that the most adequate type for {instance.name} is {result.name}.")
+            i = kg.add_instance(result, instance.name)
+            self.describe(f"Current classes of {i.name} are:")
+            for cls in i.is_a:
+                self.describe(f"\n- {cls.name}")
+
     sub_types_by_name = dict()
     replacements = {
         INSTANCE_NAME: instance.name,
         INSTANCE_NAME_FANCY: human_name(instance),
-        CLASS_NAME: root_class.name,
-        CLASS_NAME_FANCY: human_name(root_class),
         CLASS_LIST: set(),
         CLASS_LIST_FANCY: set(),
     }
-    for cls in root_class.subclasses():
+    for cls in classes:
         replacements[CLASS_LIST].add(f"'{cls.name}'")
         replacements[CLASS_LIST_FANCY].add(f"'{human_name(cls)}'")
         sub_types_by_name[cls.name] = cls
         sub_types_by_name[human_name(cls)] = cls
     replacements[CLASS_LIST] = ", ".join(replacements[CLASS_LIST])
     replacements[CLASS_LIST_FANCY] = ", ".join(replacements[CLASS_LIST_FANCY])
-    questions = [_apply_replacements(pattern, replacements) for pattern in queries]
-    for question in questions:
-        files = [kg.path]
-        for attempt in range(0, max_retries):
-            query = ai_query(question=question, attempt=attempt if attempt > 0 else None)
-            result = first_or_none(cls for name, cls in sub_types_by_name.items() if name in query.result_text)
-            if result is None:
-                logger.warning("Inconclusive answer for query '%s', AI answer: %s", query.question, query.result_text)
-                continue
-            description = f"Query: {query.question}.\nAnswer:\n\t{query.result_text}"
-            description += f"\nmeaning that the most adequate type for {instance.name} is {result.name}."
-            instance = kg.add_instance(result, instance.name)
-            description += f"Current classes of {instance.name} are:"
-            for cls in instance.is_a:
-                description += f"\n- {cls.name}"
-            files.append(query.cache_path)
-            description += f"\nQuery cache in files:"
-            for file in files[1:]:
-                description += f"\n- {str(file)}"
-            return Commit(
-                message=f"make {instance.name} an instance of classes {[cls.name for cls in instance.is_a]} from AI answer",
-                description=description,
-                files=files,
-            )
+    return _make_queries(kg, queries, MoveToMostAdequateClassQueryProcessor(), max_retries=max_retries, **replacements)
+
+
+def move_to_most_adequate_subclass(kg: KnowledgeGraph,
+                                   instance: owlready.Thing,
+                                   root_class: owlready.ThingClass,
+                                   queries: typing.List[str],
+                                   max_retries: int = DEFAULT_MAX_RETRIES) -> Commitable:
+    return move_to_most_adequate_class(kg, instance, root_class.subclasses(), queries, max_retries=max_retries)
