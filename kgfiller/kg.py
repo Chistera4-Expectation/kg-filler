fcae23edcf14505258a3acbf14a41ab1e91c3402
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-05-19 11:17:04 +0200

    fix: type testing

--- kgfiller/kg.py
+++ kgfiller/kg.py
@@ -11,31 +11,27 @@ from kgfiller.utils import *
 PATH_ONTOLOGY = PATH_DATA_DIR / "ontology.owl"
 logger.debug("ONTOLOGY_PATH = %s", PATH_ONTOLOGY.absolute())
 
-
 if not PATH_ONTOLOGY.exists():
     raise FileNotFoundError(f"ONTOLOGY_PATH {PATH_ONTOLOGY.absolute()} does not exist")
-    
 
-def ancestors(cls_or_instance: owlready.ThingClass | owlready.Thing) -> typing.Iterable[owlready.ThingClass]:
-    if isinstance(cls_or_instance, owlready.ThingClass):
-        yield from cls_or_instance.ancestors()
-    else:
-        met = set()
-        for cls in cls_or_instance.is_a:
-            if cls not in met:
-                yield from cls.ancestors()
-                met.add(cls)
+
+def subtype(cls1: owlready.ThingClass, cls2: owlready.ThingClass, strict: bool = False) -> bool:
+    return overlap(set(cls1), cls2.descendants(include_self=not strict))
+
+
+def supertype(cls1: owlready.ThingClass, cls2: owlready.ThingClass, strict: bool = False) -> bool:
+    return overlap(set(cls1), cls2.ancestors(include_self=not strict))
 
 
 def instance_of(instance: owlready.Thing, cls: owlready.ThingClass) -> bool:
-    return overlap(ancestors(instance), ancestors(cls))
+    return overlap(instance.is_a, cls.descendants())
 
 
 def human_name(cls_or_instance: owlready.ThingClass | owlready.Thing) -> str:
     return first_or_none(cls_or_instance.fancyName) or cls_or_instance.name
 
 
-def owl_name(name: str, instance: bool=True) -> str:
+def owl_name(name: str, instance: bool = True) -> str:
     name = unidecode.unidecode(name)
     name = name.strip()
     name = replace_symbols_with(name, "_")
@@ -46,19 +42,19 @@ def owl_name(name: str, instance: bool=True) -> str:
 
 
 class KnowledgeGraph:
-    def __init__(self, path: pathlib.Path=PATH_ONTOLOGY) -> None:
+    def __init__(self, path: pathlib.Path = PATH_ONTOLOGY) -> None:
         self._path = path
         self._uri = path.as_uri()
 
     @property
     def path(self) -> pathlib.Path:
         return self._path
-    
+
     @LazyProperty
     def onto(self) -> owlready.Ontology:
         logger.debug("Loading ontology from %s", self._uri)
         return owlready.get_ontology(self._uri).load()
-    
+
     def _find_root_class(self):
         things = set()
         for cls in self.onto.classes():
@@ -68,16 +64,19 @@ class KnowledgeGraph:
             return first(things)
         else:
             return owlready.Thing
-        
-    def add_property(self, cls_or_instance: owlready.ThingClass | owlready.Thing, property: str | owlready.ObjectPropertyClass, value: owlready.ThingClass | owlready.Thing | str) -> None:
+
+    def add_property(self, cls_or_instance: owlready.ThingClass | owlready.Thing,
+                     property: str | owlready.ObjectPropertyClass,
+                     value: owlready.ThingClass | owlready.Thing | str) -> None:
         if isinstance(property, owlready.ObjectPropertyClass):
             property = property.name
         property_values = getattr(cls_or_instance, property)
         if value not in property_values:
             property_values.append(value)
         logger.debug("Set property '%s' of %s to %s", property, cls_or_instance, value)
-    
-    def add_instance(self, cls: str | owlready.ThingClass, name: str, add_to_class_if_existing: bool=True) -> owlready.Thing:
+
+    def add_instance(self, cls: str | owlready.ThingClass, name: str,
+                     add_to_class_if_existing: bool = True) -> owlready.Thing:
         fancy_name = name
         name = owl_name(name)
         cls = self.onto[cls] if isinstance(cls, str) else cls
@@ -85,7 +84,10 @@ class KnowledgeGraph:
         if instance is not None:
             if add_to_class_if_existing:
                 if instance_of(instance, cls):
-                    logger.debug("Do nothing: entity %s is already instance of class %s", instance, cls)
+                    logger.debug(
+                        "Do nothing: entity %s is already instance of classes %s, "
+                        "therefore it is already an instance of %s",
+                        instance, instance.is_a, cls)
                 else:
                     instance.is_instance_of.append(cls)
                     logger.debug("Added instance %s to class %s", instance, cls)
@@ -98,7 +100,8 @@ class KnowledgeGraph:
             self.add_property(instance, "fancyName", fancy_name)
         return instance
 
-    def visit_classes_depth_first(self, root: str | owlready.ThingClass | None = None, postorder=True) -> typing.Iterable[owlready.ThingClass]:
+    def visit_classes_depth_first(self, root: str | owlready.ThingClass | None = None, postorder=True) -> \
+            typing.Iterable[owlready.ThingClass]:
         root = self._find_root_class() if root is None else root
         root = self.onto[root] if isinstance(root, str) else root
         if not postorder:
@@ -114,6 +117,6 @@ class KnowledgeGraph:
     def __enter__(self) -> "KnowledgeGraph":
         self.onto
         return self
-    
+
     def __exit__(self, exc_type, exc_value, traceback) -> None:
         self.save()
--- kgfiller/utils.py
+++ kgfiller/utils.py
@@ -6,18 +6,26 @@ def overlap(a: typing.Iterable, b: typing.Iterable) -> bool:
     met_in_b = set()
     iter_a = iter(a)
     iter_b = iter(b)
-    try:
-        while True:
-            item_a = next(iter_a)
-            if item_a in met_in_b:
-                return True
-            met_in_a.add(item_a)
-            item_b = next(iter_b)
-            if item_b in met_in_a:
-                return True
-            met_in_b.add(item_b)
-    except StopIteration:
-        return False
+    over_a = False
+    over_b = False
+    while not (over_a and over_b):
+        if not over_a:
+            try:
+                item_a = next(iter_a)
+                if item_a in met_in_b:
+                    return True
+                met_in_a.add(item_a)
+            except StopIteration:
+                over_a = True
+        if not over_b:
+            try:
+                item_b = next(iter_b)
+                if item_b in met_in_a:
+                    return True
+                met_in_b.add(item_b)
+            except StopIteration:
+                over_b = True
+    return False
 
 
 def first(iterable: typing.Iterable[typing.Any]) -> typing.Any:
