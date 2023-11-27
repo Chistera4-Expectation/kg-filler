bb1391be31e23d00b7716b4862031d63f9afdf14
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-11-27 15:16:54 +0100

    fix: itemization

--- kgfiller/text.py
+++ kgfiller/text.py
@@ -1,13 +1,17 @@
 import hashlib
 import re
 import typing
+import enum
 from dataclasses import dataclass
 import nltk
 nltk.download('wordnet')
 from nltk.corpus import wordnet
 
+
+PATTERN_BULLETED = re.compile(r"[-*+]|[#]+")
+PATTERN_NUMBERED = re.compile(r"\d+.")
 PATTERN_INLINE_LIST_ITEM = re.compile(r"(?:,\s+(?:and\s+)?)?([^,.]+)", re.IGNORECASE)
-PATTERN_LIST_ITEM = re.compile(r"^\n?(?:\d+.|[-*+]|[#]+)\s*(.*?)$", re.MULTILINE)
+PATTERN_LIST_ITEM = re.compile(r"^\n?(\d+.|[-*+]|[#]+)\s*(.*?)$", re.MULTILINE)
 PATTERN_ITEM_WITH_PARENTHESES = re.compile(r"(?:[,;])?(.+?)(?:\s+\((.+?)\))")
 PATTERN_ITEM_WITH_DETAILS = re.compile(r"(.+?)(?:(?:\s+-+\s+|:\s+)(.+))")
 PATTERN_ITEM_WITH_OR_OPTION = re.compile(r"(.+?)(?:\s+or+\s+)(.+)")
@@ -16,9 +20,11 @@ PATTERN_ITEM_WITH_OR_OPTION = re.compile(r"(.+?)(?:\s+or+\s+)(.+)")
 DEFAULT_SEPARATING_WORDS = {"and", "with", "or"}
 
 
-def split_recursively(text: str, separators: typing.List[str] = None) -> typing.Iterable[str]:
+def split_recursively(text: str, separators: typing.Iterable[str] = None) -> typing.Iterable[str]:
     if separators is None:
         separators = list(DEFAULT_SEPARATING_WORDS)
+    elif isinstance(separators, typing.Iterable) and not isinstance(separators, typing.List):
+        separators = list(separators)
     if len(separators) == 0:
         yield text.strip()
     else:
@@ -26,26 +32,70 @@ def split_recursively(text: str, separators: typing.List[str] = None) -> typing.
             yield from split_recursively(item, separators[1:])
 
 
+def _is_meaningful_word(word: str) -> bool:
+    if word:
+        return len(wordnet.synsets(word.split()[-1].lower())) > 0
+    return False
+
+
+class Prefix:
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def __str__(self) -> str:
+        return self.value
+    
+    def __repr__(self) -> str:
+        return f"Prefix('{self.value}')"
+
+    @property
+    def is_empty(self) -> bool:
+        return self.value == ""
+    
+    @property
+    def is_numbered(self) -> bool:
+        return PATTERN_NUMBERED.fullmatch(self.value) is not None
+    
+    @property
+    def is_bulleted(self) -> bool:
+        return PATTERN_BULLETED.fullmatch(self.value) is not None
+    
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, Prefix) and self.is_empty == other.is_empty and self.is_bulleted == other.is_bulleted \
+               and self.is_numbered == other.is_numbered
+    
+    def __hash__(self) -> int:
+        return hash((self.is_empty, self.is_bulleted, self.is_numbered))
+
+
 @dataclass
 class Item:
     value: str
     metadata: typing.Optional[str] = None
+    prefix: Prefix = Prefix("")
 
     @staticmethod
-    def from_string(string: str) -> typing.List["Item"]:
+    def from_string(prefix: str, string: str) -> typing.List["Item"]:
+        prefix = Prefix(prefix)
         items = []
         for match in PATTERN_ITEM_WITH_PARENTHESES.finditer(string):
-            items.append(Item(match.group(1).strip()))
+            items.append(Item(value=match.group(1).strip(), prefix=prefix))
         if '(' in string and ')' not in string:
-            items.append(Item(string.split('(')[0].strip()))
+            items.append(Item(value=string.split('(')[0].strip(), prefix=prefix))
         for match in PATTERN_ITEM_WITH_DETAILS.finditer(string):
-            items.append(Item(match.group(1).strip()))
+            items.append(Item(value=match.group(1).strip(), prefix=prefix))
         if len(items) == 0:
-            items.append(Item(string.strip()))
+            items.append(Item(value=string.strip(), prefix=prefix))
         return items
     
-    def split_by_words(self, words: typing.List[str] = None) -> typing.List[str]:
-        if words is not None and any(word in self.value for word in words):
+    def is_meaningful(self) -> bool:
+        return _is_meaningful_word(self.value.split()[-1])
+    
+    def split_by_words(self, words: typing.Iterable[str] = None) -> typing.List[str]:
+        if words is None:
+            words = DEFAULT_SEPARATING_WORDS
+        words = [f" {w} " for w in words]
+        if any(word in self.value for word in words):
             return list(split_recursively(self.value, words))
         else:
             return [self.value]
@@ -59,32 +109,18 @@ def str_hash(input: str, hash_function = 'sha256') -> str:
     return hashf(input.encode("utf-8")).hexdigest()
 
 
-def _listify_lines(text: str) -> typing.List[str]:
-    items = PATTERN_LIST_ITEM.findall(text)
-    if PATTERN_ITEM_WITH_PARENTHESES.match(items[-1].lower()):
-        items[-1] = items[-1].split('(')[0].strip()
-    elif '(' in items[-1] and ')' not in items[-1]:
-        items[-1] = items[-1].split('(')[0].strip()
-    elif PATTERN_ITEM_WITH_DETAILS.match(items[-1].lower()):
-        items[-1] = items[-1].split('-')[0].split(':')[0].strip()
-    elif len(wordnet.synsets(items[-1].lower().split()[-1])) == 0:  
-        items = items[:-1]
-    else:
-        pass
-    for i in range(len(items)):
-        if PATTERN_ITEM_WITH_OR_OPTION.match(items[i].lower()):
-            items[i] = items[i].split(' or ')[0].strip()
-    return items
+def _listify_lines(text: str) -> typing.List[typing.Tuple[str, str]]:
+    return PATTERN_LIST_ITEM.findall(text)
 
 
-def _listify_line(text: str) -> typing.List[str]:
+def _listify_line(text: str) -> typing.List[typing.Tuple[str, str]]:
     items = []
     for match in PATTERN_INLINE_LIST_ITEM.finditer(text):
-        items.append(match.group(1).strip())
+        items.append(("", match.group(1).strip()))
     return items
 
 
-def listify(text: str) -> typing.List[str]:
+def listify(text: str) -> typing.List[typing.Tuple[str, str]]:
     if "\n" in text:
         return _listify_lines(text)
     elif PATTERN_LIST_ITEM.fullmatch(text):
@@ -93,8 +129,16 @@ def listify(text: str) -> typing.List[str]:
         return _listify_line(text)
 
 
+def _itemize(text: str) -> typing.Iterable[Item]:
+    for prefix_item in listify(text):
+        for item in Item.from_string(*prefix_item):
+            for word in item.split_by_words():
+                yield Item(value=word, prefix=item.prefix)
+
+
 def itemize(text: str) -> typing.List[Item]:
-    items = []
-    for item in listify(text):
-        items.extend(Item.from_string(item))
+    items = list(_itemize(text))
+    if len(items) >= 2 and items[0].prefix != items[1].prefix:
+        items = items[1:]
+    items = [item for item in items if item.is_meaningful()]
     return items
--- tests/test_postprocess.py
+++ tests/test_postprocess.py
@@ -10,97 +10,97 @@ class TestItemize(unittest.TestCase):
         results = itemize(query_result)
         expected_items = ['Flour', 'Yeast', 'Sugar', 'Salt', 'Eggs', 'Milk', 'Butter']
         for index, result in enumerate(results):
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
         
         query_result = "1. Beef sirloin\n\n2. Soy sauce\n\n3. Brown sugar\n\n4. Sesame oil\n\n5. Garlic\n\n6. Ginger\n\n7. Green onions\n\n8. Black pepper\n\n9. Rice wine\n\n10. Sesame seeds"
         results = itemize(query_result)
         expected_items = ['Beef sirloin', 'Soy sauce', 'Brown sugar', 'Sesame oil', 'Garlic', 'Ginger', 'Green onions', 'Black pepper', 'Rice wine', 'Sesame seeds']
         for index, result in enumerate(results):
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
 
     def test_lengthy_elements(self):
         query_result = "- Green curry paste\n\n- Coconut milk\n\n- Chicken or tofu\n\n- Thai eggplant\n\n- Bamboo shoots\n\n- Green beans\n\n- Kaffir lime leaves\n\n- Thai basil leaves\n\n- Fish sauce\n\n- Palm sugar\n\n- Green chili peppers"
         results = itemize(query_result)
-        expected_items = ['Green curry paste', 'Coconut milk', 'Chicken', 'Thai eggplant', 'Bamboo shoots', 'Green beans', 'Kaffir lime leaves', 'Thai basil leaves', 'Fish sauce', 'Palm sugar', 'Green chili peppers']
+        expected_items = ['Green curry paste', 'Coconut milk', 'Chicken', 'tofu', 'Thai eggplant', 'Bamboo shoots', 'Green beans', 'Kaffir lime leaves', 'Thai basil leaves', 'Fish sauce', 'Palm sugar', 'Green chili peppers']
         for index, result in enumerate(results):
             print(result.value)
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
 
     def test_inline_list(self):
         query_result = "tomatoes, onion, jalapeno pepper, cilantro, lime juice, salt."
         results = itemize(query_result)
         expected_items = ['tomatoes', 'onion', 'jalapeno pepper', 'cilantro', 'lime juice', 'salt']
         for index, result in enumerate(results):
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
 
     def test_text_before_list(self):
         query_result = "ingredients are:\n\n1. Beef sirloin\n\n2. Soy sauce\n\n3. Brown sugar\n\n4. Sesame oil\n\n5. Garlic\n\n6. Ginger\n\n7. Green onions\n\n8. Black pepper\n\n9. Rice wine\n\n10. Sesame seeds"
         results = itemize(query_result)
         expected_items = ['Beef sirloin', 'Soy sauce', 'Brown sugar', 'Sesame oil', 'Garlic', 'Ginger', 'Green onions', 'Black pepper', 'Rice wine', 'Sesame seeds']
         for index, result in enumerate(results):
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
 
         query_result = "possible ingredients can be:\n\n- tomatoes\n\n- onion\n\n- jalapeno pepper\n\n- cilantro\n\n- lime juice\n\n- salt"
         results = itemize(query_result)
         expected_items = ['tomatoes', 'onion', 'jalapeno pepper', 'cilantro', 'lime juice', 'salt']
         for index, result in enumerate(results):
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
 
     def test_weird_last_element(self):
         query_result = "- Olive oil\n\n- Onion\n\n- Garlic\n\n- Carrots\n\n- Celery\n\n- Zucchini\n\n- Potatoes\n\n- Tomatoes\n\n- Tomato paste\n\n- Vegetable broth\n\n- Cannellini beans\n\n- Green beans\n\n- Pasta\n\n- Salt\n\n- Pepper\n\n- Basil\n\n- Parmesan cheese (optional)"
         results = itemize(query_result)
         expected_items = ['Olive oil', 'Onion', 'Garlic', 'Carrots', 'Celery', 'Zucchini', 'Potatoes', 'Tomatoes', 'Tomato paste', 'Vegetable broth', 'Cannellini beans', 'Green beans', 'Pasta', 'Salt', 'Pepper', 'Basil', 'Parmesan cheese']
         for index, result in enumerate(results):
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
 
         query_result = "- Olive oil\n\n- Onion\n\n- Garlic\n\n- Carrots\n\n- Celery\n\n- Zucchini\n\n- Potatoes\n\n- Tomatoes\n\n- Tomato paste\n\n- Vegetable broth\n\n- Cannellini beans\n\n- Green beans\n\n- Pasta\n\n- Salt\n\n- Pepper\n\n- Basil\n\n- Parmesan cheese : optional"
         results = itemize(query_result)
         expected_items = ['Olive oil', 'Onion', 'Garlic', 'Carrots', 'Celery', 'Zucchini', 'Potatoes', 'Tomatoes', 'Tomato paste', 'Vegetable broth', 'Cannellini beans', 'Green beans', 'Pasta', 'Salt', 'Pepper', 'Basil', 'Parmesan cheese']
         for index, result in enumerate(results):
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
 
         query_result = "- Olive oil\n\n- Onion\n\n- Garlic\n\n- Carrots\n\n- Celery\n\n- Zucchini\n\n- Potatoes\n\n- Tomatoes\n\n- Tomato paste\n\n- Vegetable broth\n\n- Cannellini beans\n\n- Green beans\n\n- Pasta\n\n- Salt\n\n- Pepper\n\n- Basil\n\n- Parmesan cheese (optional) : 50gr"
         results = itemize(query_result)
         expected_items = ['Olive oil', 'Onion', 'Garlic', 'Carrots', 'Celery', 'Zucchini', 'Potatoes', 'Tomatoes', 'Tomato paste', 'Vegetable broth', 'Cannellini beans', 'Green beans', 'Pasta', 'Salt', 'Pepper', 'Basil', 'Parmesan cheese']
         for index, result in enumerate(results):
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
 
     def test_or_in_element(self):
         query_result = "1. Whole wheat flour\n\n2. Water\n\n3. Yeast\n\n4. Salt\n\n5. Honey or sugar\n\n6. Olive oil or butter"
         results = itemize(query_result)
-        expected_items = ['Whole wheat flour', 'Water', 'Yeast', 'Salt', 'Honey', 'Olive oil']
+        expected_items = ['Whole wheat flour', 'Water', 'Yeast', 'Salt', 'Honey', 'sugar', 'Olive oil', 'butter']
         for index, result in enumerate(results):
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
 
     def test_optional_elements(self):
         query_result = "1. Whole wheat flour\n\n2. Water\n\n3. Yeast\n\n4. Salt\n\n5. Honey (optional)\n\n6. Olive oil (optional)"
         results = itemize(query_result)
         expected_items = ['Whole wheat flour', 'Water', 'Yeast', 'Salt', 'Honey', 'Olive oil']
         for index, result in enumerate(results):
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
 
         query_result = "- Tortillas\n\n- Enchilada sauce\n\n- Shredded cheese\n\n- Cooked chicken (optional)\n\n- Diced onions (optional)\n\n- Diced tomatoes (optional)\n\n- Sliced black olives (optional)\n\n- Sour cream (optional)"
         results = itemize(query_result)
         expected_items = ['Tortillas', 'Enchilada sauce', 'Shredded cheese', 'Cooked chicken', 'Diced onions', 'Diced tomatoes', 'Sliced black olives', 'Sour cream']
         for index, result in enumerate(results):
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
 
     def test_parenthesis(self):
         query_result = "1. Beef (sirloin)\n\n2. Soy sauce\n\n3. Brown sugar (raw)\n\n4. Sesame oil\n\n5. Garlic\n\n6. Ginger\n\n7. Green onions (peeled)\n\n8. Black pepper\n\n9. Rice wine\n\n10. Sesame seeds"
         results = itemize(query_result)
         expected_items = ['Beef', 'Soy sauce', 'Brown sugar', 'Sesame oil', 'Garlic', 'Ginger', 'Green onions', 'Black pepper', 'Rice wine', 'Sesame seeds']
         for index, result in enumerate(results):
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
 
     def test_unclosed_parenthesis(self):
         query_result = "1. Beef (usually thinly sliced ribeye\n\n2. Soy sauce\n\n3. Brown sugar (raw\n\n4. Sesame oil\n\n5. Garlic\n\n6. Ginger\n\n7. Green onions\n\n8. Black pepper\n\n9. Rice wine\n\n10. Sesame seeds"
         results = itemize(query_result)
         expected_items = ['Beef', 'Soy sauce', 'Brown sugar', 'Sesame oil', 'Garlic', 'Ginger', 'Green onions', 'Black pepper', 'Rice wine', 'Sesame seeds']
         for index, result in enumerate(results):
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
 
         query_result = "1. Soy sauce\n\n2. Brown sugar (raw\n\n3. Sesame oil\n\n4. Garlic\n\n5. Ginger\n\n6. Green onions\n\n7. Black pepper\n\n8. Rice wine\n\n9. Sesame seeds\n\n10. beef (sirloin"
         results = itemize(query_result)
         expected_items = ['Soy sauce', 'Brown sugar', 'Sesame oil', 'Garlic', 'Ginger', 'Green onions', 'Black pepper', 'Rice wine', 'Sesame seeds', 'beef']
         for index, result in enumerate(results):
-            assert result.value == expected_items[index]
+            self.assertEqual(result.value, expected_items[index])
