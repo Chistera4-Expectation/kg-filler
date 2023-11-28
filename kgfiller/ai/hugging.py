675ed3e9599bbc85d0a66500e845f064658d240e
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-11-28 13:08:32 +0100

    chore: suggestions on hugging

--- kgfiller/ai/hugging.py
+++ kgfiller/ai/hugging.py
@@ -8,6 +8,7 @@ import yaml
 
 from kgfiller import logger, unescape
 import kgfiller.ai as ai
+from tempfile import mkdtemp
 
 
 DEFAULT_MODEL = "hugging_mistral"
@@ -41,7 +42,6 @@ class HuggingAiStats:
 stats = HuggingAiStats()
 
 
-@dataclass
 class HuggingAiQuery(ai.AiQuery):
     def __init__(self, **kwargs):
         if "model" not in kwargs or kwargs["model"] is None:
@@ -50,25 +50,28 @@ class HuggingAiQuery(ai.AiQuery):
             kwargs["background"] = DEFAULT_BACKGROUND
         super().__init__(**kwargs)
 
-    def _chat_completion_step(self):
+    def _create_chatbot(self):
         sign = Login(username, password)
         cookies = sign.login()
-        cookie_path_dir = "./cookies_snapshot"
+        cookie_path_dir = mkdtemp("hugging-cookies")
         sign.saveCookiesToDir(cookie_path_dir)
-        chat_model = hugchat.ChatBot(cookies=cookies.get_dict())
-        id = chat_model.new_conversation()
-        chat_model.change_conversation(id)
-        for index, av_model in enumerate(chat_model.get_available_llm_models()):
+        return hugchat.ChatBot(cookies=cookies.get_dict())
+
+    def _select_llm(self, chat_bot):
+        chosen_model_index = None
+        for index, av_model in enumerate(chat_bot.get_available_llm_models()):
             if self.model.split('_')[-1].lower() in av_model.name.lower():
                 chosen_model_index = index
-        returned = chat_model.switch_llm(chosen_model_index)
-        id = chat_model.new_conversation()
-        chat_model.change_conversation(id)
-        info = chat_model.get_conversation_info()
-        context = "You're a dietician."
-        chat_model.query(context)
-        result = chat_model.query(self.question,
-                                truncate=self.limit)
+        if chosen_model_index is not None:
+            chat_bot.switch_llm(chosen_model_index)
+
+    def _chat_completion_step(self):
+        chat_bot = self._create_chatbot()
+        self._select_llm(chat_bot)
+        id = chat_bot.new_conversation()
+        chat_bot.change_conversation(id)
+        result = chat_bot.query(self.background + ".\n" + self.question, truncate=self.limit)
+        result.wait_until_done()
         return result
 
     @classmethod
@@ -76,6 +79,7 @@ class HuggingAiQuery(ai.AiQuery):
         return hugchat.exceptions.ModelOverloadedError
 
     def _chat_completion_to_yaml(self) -> str:
+        # TODO @andagio dobbiamo guardarci insieme qui
         return yaml.safe_load(str(self._chat_completion))
 
     def _extract_text_from_result(self) -> str:
