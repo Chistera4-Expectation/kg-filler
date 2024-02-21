59cc027c4533e2414897cc9d50bcff329f3c51de
AndAgio <aagiollo@gmail.com>
2024-02-22 00:30:26 +0100

    fix: consider only instances of current class and not all subclasses

--- kgfiller/__main__.py
+++ kgfiller/__main__.py
@@ -2,7 +2,7 @@ from kgfiller import logger
 from kgfiller.git import DataRepository
 from kgfiller.kg import subtype
 from kgfiller.strategies import *
-from kgfiller.text import gather_possible_duplicates
+from kgfiller.text import gather_possible_duplicates, gather_all_instances_of_class_without_subclasses
 from kgfiller.utils import load_queries_yaml
 
 
@@ -17,23 +17,30 @@ duplicates_queries = queries['duplicate']
 with DataRepository() as repo:
     with KnowledgeGraph() as kg:
         Recipe = kg.onto.Recipe
+        logger.debug('Step 1. Finding food instances...')
         for cls in kg.visit_classes_depth_first():
             if not subtype(cls, Recipe):
+                logger.debug('Step 1. Checking class "{}"...'.format(cls))
                 commit = find_instances_for_class(kg, cls, instance_queries)
                 kg.save()
                 repo.maybe_commit(commit)
+        logger.debug('Step 2. Finding recipe instances...')
         commit = find_instances_for_recipes(kg, Recipe, recipe_queries)
         kg.save()
         repo.maybe_commit(commit)
+        logger.debug('Step 3. Finding relation instances...')
         for instance in kg.onto.Recipe.instances():
+            logger.debug('Step 3. Checking instance "{}"...'.format(instance))
             commit = find_related_instances(kg, instance, kg.onto.ingredientOf, kg.onto.Edible, relation_queries, instance_as_object=True)
             kg.save()
             repo.maybe_commit(commit)
+        logger.debug('Step 4. Refining position of food instances...')
         already_met_instances = set()
         for cls in kg.visit_classes_depth_first():
             if not is_leaf(cls) and not subtype(cls, Recipe):
-                for instance in cls.instances():
+                for instance in gather_all_instances_of_class_without_subclasses(cls):
                     if instance not in already_met_instances:
+                        logger.debug('Step 4. Checking instance "{}" in class "{}"...'.format(instance, cls))
                         already_met_instances.add(instance)
                         commit = move_to_most_adequate_subclass(kg, instance, cls, leaf_descendants, rebalance_queries)
                         if not commit.should_commit:
@@ -44,12 +51,13 @@ with DataRepository() as repo:
                         commit.should_commit = True
                         kg.save()
                         repo.maybe_commit(commit)
+        logger.debug('Step 5. Checking duplicate food instances...')
         for cls in kg.visit_classes_depth_first():
             if not subtype(cls, Recipe):
                 possible_duplicates = gather_possible_duplicates(cls)
-                logger.debug('Possible duplicates in class "{}" are: {}'.format(cls, possible_duplicates))
+                # logger.debug('Step 5. Possible duplicates in class "{}" are: {}'.format(cls, possible_duplicates))
                 for possible_duplicates_couple in possible_duplicates:
-                    logger.debug('Checking couple {}'.format(possible_duplicates_couple))
+                    logger.debug('Step 5. Checking couple "{}" in class "{}"...'.format(possible_duplicates_couple, cls))
                     commit = check_duplicates(kg, cls, possible_duplicates_couple, duplicates_queries)
                     kg.save()
                     repo.maybe_commit(commit)
